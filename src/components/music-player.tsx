65d9d21008284aba3141e9636c6625c2dbd3fd02
Mario Aleman <mar.alemanf@gmail.com>
2024-06-09 20:51:55 -0600

    tracking queue music

--- src/components/music-player.tsx
+++ src/components/music-player.tsx
@@ -4,27 +4,54 @@ import { Animated, Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableO
 import IonIcons from 'react-native-vector-icons/Ionicons'
 import songs, { TSongs } from "./utils/mocks"
 
+import TrackPlayer, { State, usePlaybackState, useProgress } from 'react-native-track-player'
+
 const { width: widthStyle } = Dimensions.get('window')
+
+const setupPlayer = async () => {
+  await TrackPlayer.setupPlayer();
+  await TrackPlayer.add(songs);
+ };
+ 
+ 
+ const togglePlayBack = async () => {
+   let trackIndex = await TrackPlayer.getActiveTrackIndex();
+   let trackObject = await TrackPlayer.getTrack(trackIndex ?? 0);
+   const playback = await TrackPlayer.getPlaybackState();
+ 
+   console.log(`Title: ${trackObject?.title}`);
+   console.log('playback', playback);
+   console.log('trackIndex', trackIndex);
+ 
+   if (trackIndex !== null) {
+     if (playback.state === State.Playing) {
+       console.log('The player is playing');
+       TrackPlayer.pause();
+     } else {
+       console.log('The player is not playing');
+       TrackPlayer.play();
+     }
+   }
+ };
 
 const MusicPlayer = () => {
   const { width } = useWindowDimensions()
-
   const scrollX = useRef(new Animated.Value(0)).current
-
   const [songIndex, setSongIndex] = useState(0)
-
   const songSlider = useRef(null)
 
   useEffect(() => {
     scrollX.addListener(({ value }) => {
       const index = Math.round(value / width)
+      console.log(index)
+      skipTo(index)
       setSongIndex(index)
     })
   })
 
   const renderSongs = ({ item, index}: { item: TSongs, index: number }) => {
     return (
-      <Animated.View  style={{ width, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
+      <Animated.View  style={{ width, justifyContent: 'center', alignItems: 'center' }} key={index}>
         <View style={styles.contentImage}>
           <Image 
             source={{ uri: item.image }}
@@ -40,9 +67,21 @@ const MusicPlayer = () => {
     songSlider.current.scrollToOffset({ offset: (songIndex + 1) * width })
   }
 
+  const skipTo = async (trackId: number) => {
+    await TrackPlayer.skip(trackId)
+  } 
+
   const skipToPrevious = () => {
     songSlider.current.scrollToOffset({ offset: (songIndex - 1) * width })
   }
+
+  const playbackState = usePlaybackState();
+  const progress = useProgress()
+  console.log('playbackState', playbackState, progress.position);
+
+  useEffect(() => {
+    setupPlayer();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -71,38 +110,38 @@ const MusicPlayer = () => {
         <View style={styles.slider}>
           <Slider 
             style={styles.progressBar}
-            value={10}
+            value={progress.position}
             minimumValue={0}
-            maximumValue={100}
+            maximumValue={progress.duration}
             thumbTintColor="white"
             minimumTrackTintColor="#ffd369"
             maximumTrackTintColor="white"
             onSlidingComplete={
-              () => {
-                console.log('Hola')
+              async (value) => {
+                await TrackPlayer.seekTo(value)
               }
             }
           />
           <View style={styles.progressLabelWrapper}>
-            <Text style={styles.progressLabelText}>0:00</Text>
-            <Text style={styles.progressLabelText}>0:00</Text>
+            <Text style={styles.progressLabelText}>{new Date(progress.position * 1000).toISOString().substr(14, 5)}</Text>
+            <Text style={styles.progressLabelText}>{new Date((progress.duration - progress.position) * 1000).toISOString().substr(14, 5)}</Text>
           </View>
         </View>
         <View style={styles.controlsPlayer}>
           <TouchableOpacity
             onPress={() => skipToPrevious()}
           >
-            <IonIcons name="play-skip-back-outline" size={24} color="white" />
+            <IonIcons name="play-skip-back-outline" size={34} color="white" />
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => console.log('Hola')}
+            onPress={() => togglePlayBack(playbackState)}
           >
-            <IonIcons name="play-outline" size={24} color="white" />
+            <IonIcons name={playbackState.state === State.Playing ? 'pause-outline' : 'play-outline'} size={74} color="white" />
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => skipForward()}
           >
-            <IonIcons name="play-skip-forward-outline" size={24} color="white" />
+            <IonIcons name="play-skip-forward-outline" size={34} color="white" />
           </TouchableOpacity>
         </View>
       </View>
@@ -142,7 +181,7 @@ const MusicPlayer = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111'
+    backgroundColor: '#0d0d0d'
   },
   wrapper: {
     flex: 1,
@@ -165,6 +204,7 @@ const styles = StyleSheet.create({
   controlsPlayer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     width: '80%',
     marginTop: 16
   },
--- src/components/utils/mocks.ts
+++ src/components/utils/mocks.ts
@@ -5,7 +5,8 @@ export type TSongs = {
     title: string,
     artist: string,
     album: string,
-    image: string
+    image: string,
+    url: string
 }
 
 const songs = [
@@ -14,63 +15,72 @@ const songs = [
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/smartest.mp3')
   },
   {
     id: 2,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/anti.mp3')
   },
   {
     id: 3,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/music-1.mp3')
   },
   {
     id: 4,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/music-2.mp3')
   },
   {
     id: 5,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/smartest.mp3')
   },
   {
     id: 6,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/anti.mp3')
   },
   {
     id: 7,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/music-1.mp3')
   },
   {
     id: 8,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/music-2.mp3')
   },
   {
     id: 9,
     title: faker.music.songName(),
     artist: faker.person.firstName('male'),
     album: faker.music.genre(),
-    image: faker.image.url()
+    image: faker.image.url(),
+    url: require('./music/smartest.mp3')
   }
 ]
 
